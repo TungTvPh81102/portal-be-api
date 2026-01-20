2be57e89bf2cdf603f7a96f5363cabf52fab3402
ZhangWenSong <quaixe121811@gmail.com>
2026-01-21 00:30:15 +0700

    fixed Cannot find module '@/config/env'

--- src/config/paths.js
+++ src/config/paths.js
@@ -3,11 +3,13 @@ const moduleAlias = require('module-alias');
 const path = require('path');
 
 // Register aliases
+// When this file is in dist/src/config, __dirname is /path/to/dist/src/config
+// So we need to go up two levels to get to the dist directory
 moduleAlias.addAliases({
-  '@': path.resolve(__dirname, '../../dist/src')
+  '@': path.resolve(__dirname, '../..')
 });
 
 // Export for potential use elsewhere
 module.exports = {
-  '@': path.resolve(__dirname, '../../dist/src')
+  '@': path.resolve(__dirname, '../..')
 };
